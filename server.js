bc5405f99df970168e36acbac6278cdd0ea778d2
JOSHUA MUJAKARI <joshuamujakari15@gmail.com>
2026-05-24 14:16:59 -0700

    fix: downgrade to Express 4.x and use simple wildcard route for compatibility

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // All other routes should return index.html (client‑side routing)
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
